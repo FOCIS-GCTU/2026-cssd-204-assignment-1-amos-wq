9cb4d665420b6d9bc3a44a8841e3f3388ebbe4e4
amos-wq <2425403496@live.gctu.edu.gh>
2026-05-31 22:17:37 +0000

    initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Amos Dogbey
+# STUDENT ID: 2425403496
+# Email: 2425403496@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Amos Dogbey, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints the small initials and stylized large block initials AAD with precise spacing.
+    print()
+    print("...AAD")
+    print()
+    print(".......AAAA................AAAA............DDDDDDDDDD........")
+    print("......AAAAAA..............AAAAAA...........DDDDDDDDDDDD......")
+    print(".....AAAAAAAA............AAAAAAAA..........DD........DDD.....")
+    print("....AAAA..AAAA..........AAAA..AAAA.........DD.........DD.....")
+    print("...AAAA....AAAA........AAAA....AAAA........DD.........DD.....")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........DD.........DD.....")
+    print("...AAAAAAAAAAAA........AAAAAAAAAAAA........DD.........DD.....")
+    print("...AAAA....AAAA........AAAA....AAAA........DD........DDD.....")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...DDDDDDDDDDDD....**")
+    print("...AAAA....AAAA...**...AAAA....AAAA...**...DDDDDDDDDD......**")
+    print()
 
 
 main()
